09bb81b06ca8102c4092b4ac91540d9b3001aa93
Vas <va1524432@gmail.com>
2022-09-24 20:21:31 +0300

    Revert "Звездная обнова"
    This reverts commit b375e266edaae63127de21cc7b3768b5f1b97d05.

--- Scripts/Player/IPlayerMovement.cs
+++
@@ -1,9 +0,0 @@
-﻿using Assets.Scripts.Common.Interfaces;
-
-namespace Assets.Scripts.Player {
-    public interface IPlayerMovement  : IGameObject {
-        float TotalSpeed { get; }
-        void StopGameHandler();
-        void Test(string message);
-    }
-}
